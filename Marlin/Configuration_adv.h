12367cc99dec1bf00b622d9eecb3eb5036a071b5
esa-n <moonrise256@gmail.com>
2020-09-20 10:29:21 +0900

    KP3: Enable LCD_SHOW_E_TOTAL

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1131,7 +1131,7 @@
 //#define LCD_SET_PROGRESS_MANUALLY
 
 // Show the E position (filament used) during printing
-//#define LCD_SHOW_E_TOTAL
+#define LCD_SHOW_E_TOTAL
 
 #if ENABLED(SHOW_BOOTSCREEN)
   #define BOOTSCREEN_TIMEOUT 4000        // (ms) Total Duration to display the boot screen(s)
