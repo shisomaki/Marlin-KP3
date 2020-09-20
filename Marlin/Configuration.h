3e8c0103c0c0b33d2eeffa8a852c32a57ba14b54
esa-n <moonrise256@gmail.com>
2020-09-20 11:10:24 +0900

    KP3: Adjust DEFAULT_Kp
    My optimum value is Kp10, but look at the margin.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -652,11 +652,11 @@
   #if ENABLED(PID_PARAMS_PER_HOTEND)
     // Specify up to one value per hotend here, according to your setup.
     // If there are fewer values, the last one applies to the remaining hotends.
-    #define DEFAULT_Kp_LIST {  24.00,  22.20 }
+    #define DEFAULT_Kp_LIST {  14.00,  22.20 }
     #define DEFAULT_Ki_LIST {   0.88,   1.08 }
     #define DEFAULT_Kd_LIST {  80.00, 114.00 }
   #else
-    #define DEFAULT_Kp  24.00
+    #define DEFAULT_Kp  14.00
     #define DEFAULT_Ki   0.88
     #define DEFAULT_Kd  80.00
   #endif
