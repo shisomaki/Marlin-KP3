24e505bbe1337c43a0e2b2ab1586b10b25901654
esa-n <moonrise256@gmail.com>
2020-09-20 11:10:24 +0900

    KP3: Adjust DEFAULT_Kp
    My optimum value is Kp10, but look at the margin.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -513,11 +513,11 @@
   #if ENABLED(PID_PARAMS_PER_HOTEND)
     // Specify between 1 and HOTENDS values per array.
     // If fewer than EXTRUDER values are provided, the last element will be repeated.
-    #define DEFAULT_Kp_LIST {  24.00,  20.00 }
+    #define DEFAULT_Kp_LIST {  14.00,  20.00 }
     #define DEFAULT_Ki_LIST {   0.88,   1.00 }
     #define DEFAULT_Kd_LIST {  80.00, 112.00 }
   #else
-    #define DEFAULT_Kp  24.00
+    #define DEFAULT_Kp  14.00
     #define DEFAULT_Ki   0.88
     #define DEFAULT_Kd  80.00
   #endif
