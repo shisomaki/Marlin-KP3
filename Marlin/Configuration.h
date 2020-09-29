c9ac1315a0519b72f89ffb31c7106b6a4d9e7855
esa-n <moonrise256@gmail.com>
2020-09-29 18:28:48 +0900

    KP3: Enable PID_AUTOTUNE_MENU

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -570,7 +570,7 @@
 
 #if ENABLED(PIDTEMP)
   #define PID_EDIT_MENU         // Add PID editing to the "Advanced Settings" menu. (~700 bytes of PROGMEM)
-  //#define PID_AUTOTUNE_MENU     // Add PID auto-tuning to the "Advanced Settings" menu. (~250 bytes of PROGMEM)
+  #define PID_AUTOTUNE_MENU     // Add PID auto-tuning to the "Advanced Settings" menu. (~250 bytes of PROGMEM)
   //#define PID_PARAMS_PER_HOTEND // Uses separate PID parameters for each extruder (useful for mismatched extruders)
                                   // Set/get with gcode: M301 E[extruder number, 0-2]
 
