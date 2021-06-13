f9af6f4eb9f074d1f1ad647b6bda517e3cfb6d05
esa-n <moonrise256@gmail.com>
2021-06-13 20:01:56 +0900

    Limit Y-axis acceleration to 200mm/s

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -917,7 +917,7 @@
  * Override with M201
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 3000, 500, 500, 10000 }
+#define DEFAULT_MAX_ACCELERATION      { 3000, 200, 500, 10000 }
 
 //#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
